a6e07bf6a8a2e7316cba8284ddb34348857b4149
clhw0112 <chbw0112@gmail.com>
2026-05-21 21:54:44 +0900

    practice

--- 4th_day.py
+++ 4th_day.py
@@ -1,6 +1,7 @@
 import json
-data = {"animal":"tiger", "gender":"female", "height": 5.2 }
-json_str = json.dumps(data, indent=2, ensure_ascii=False)
-print(json_str)
-j = json.loads(json_str)
-print(j)
+data = {"name": "Choi", "lan":"kor", "gen":"m"}
+with open("json_practice", "w", encoding="utf - 8") as f:
+    json.dump(data, f, indent=2)
+with open("json_practice", "r", encoding="utf - 8") as f:
+    load = json.load(f)
+print(load["lan"])
